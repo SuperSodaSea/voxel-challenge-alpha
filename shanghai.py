d097f75817c38debe9c21f1b7af99a297bfec96b
SuperSodaSea <bobby825@126.com>
2022-05-03 16:55:01 +0800

    Update Shanghai

--- shanghai.py
+++ shanghai.py
@@ -1,9 +1,9 @@
 from scene import Scene; import taichi as ti; from taichi.math import *
 
-scene = Scene(0, 1.2)
+scene = Scene(0, 1.25)
 scene.set_background_color((0.6, 0.8, 1.0))
 scene.set_floor(-1, (1.3, 1.6, 2.0))
-scene.set_directional_light((1, 1, 0.75), 0.7, (1.0, 0.9, 0.8))
+scene.set_directional_light((1, 1, 0.75), 0.5, (1.0, 0.9, 0.8))
 
 @ti.func
 def set(idx, mat, color, noise = vec3(0)): scene.set_voxel(idx, mat, color + ti.random() * noise)
@@ -26,6 +26,31 @@ def coneY(p0, p1, r0, r1, mat, color, noise = vec3(0)):
 
 @ti.kernel
 def initialize():
+    for x, z in ti.ndrange((-64, 64), (-64, 64)):
+        p1 = vec2(32 - x, 35 - z); p1 = vec2(dot(p1, vec2(1.1, -1)), dot(p1, vec2(1, 1))) / ti.sqrt(2)
+        h = 0.05 * p1.y - 0.00075 * p1.x * p1.x + 0.2 * (ti.sin(0.06 * x + 0.11 * z) + ti.sin(0.09 * x + 0.07 * z))
+        if h >= 0: fill(ivec3(x, -64, z), ivec3(1, 4 + h, 1), 1, vec3(0.25, 0.5, 0.25), 0.1)
+    for x, z in ti.ndrange((-64, 64), (-64, 64)):
+        if scene.get_voxel(ivec3(x, -64, z))[0] == 0:
+            for x1, z1 in ti.ndrange((x - 14, x + 15), (z - 14, z + 15)):
+                if (x1 >= -64 and x1 < 64 and z1 >= -64 and z1 < 64 and (x1 - x) ** 2 + (z1 - z) ** 2 <= 14 ** 2 and
+            scene.get_voxel(ivec3(x1, -64, z1))[0] > 0): set(ivec3(x, -63, z), 1, vec3(0.05, 0.1, 0.3), vec3(0, 0, 0.1))
+    for x, z in ti.ndrange((-64, 64), (-64, 64)):
+        if scene.get_voxel(ivec3(x, -64, z))[0] == 0 and scene.get_voxel(ivec3(x, -63, z))[0] > 0:
+            set(ivec3(x, -64, z), 1, vec3(0.05, 0.1, 0.3), vec3(0, 0, 0.1))
+        else:
+            fill(ivec3(x, -64, z), ivec3(1, 3, 1), 1, vec3(0.7, 0.5, 0.3), 0.1)
+            set(ivec3(x, -61, z), 1, vec3(0.25, 0.5, 0.25), 0.1)
+    for x, z in ti.ndrange((-64, 64), (-64, 64)):
+        for x1, z1 in ti.ndrange((x - 4, x + 5), (z - 4, z + 5)):
+            if (x1 >= -64 and x1 < 64 and z1 >= -64 and z1 < 64 and (x1 - x) ** 2 + (z1 - z) ** 2 <= 4 ** 2
+            and scene.get_voxel(ivec3(x1, -62, z1))[0] == 0): set(ivec3(x, -61, z), 0, vec3(0))
+    for x, z in ti.ndrange((-64, 64), (-64, 64)):
+        y = -64
+        while y < 64 and scene.get_voxel(ivec3(x, y, z))[0] > 0: y += 1
+        if y <= -64 + 2 and ti.random() < 0.02: set(ivec3(x, y - 1, z), 2, vec3(0.3), 0.7)
+        elif y >= -64 + 4 and ti.random() < 0.008: set(ivec3(x, y, z), 1, vec3(0.8, 0.8 * ti.random() ** 4, 0), 0.2)
+    
     c = ivec3(8, 0, 16)
     sphere(c + vec3(0, -32, 0), 11, 1, vec3(0.6), 0.2)
     fill(c + ivec3(-16, -35, -16), ivec3(33, 6, 33), 1, vec3(0.6, 0.1, 0.1), paint = True)
@@ -59,24 +84,5 @@ def initialize():
         if ti.abs(p.x) + ti.abs(p.z) <= 14 and ti.abs(p.z) <= ((63 - p.y) / 127) ** 0.7 * 15 + 1: set(c + p, 1,
             vec3(0.1, 0.2, 0.3) if p.z == 0 else vec3(0.4, 0.6, 0.8) if p.y % 4 == 0 else vec3(0.2, 0.4, 0.6), 0.1)
     fill(c + ivec3(-10, 46, -16), ivec3(20, 12, 32), 0, vec3(0))
-    
-    for x, z in ti.ndrange((-64, 64), (-64, 64)):
-        p1 = vec2(32 - x, 35 - z); p1 = vec2(dot(p1, vec2(1.1, -1)), dot(p1, vec2(1, 1))) / ti.sqrt(2)
-        h = 0.05 * p1.y - 0.00075 * p1.x * p1.x + 0.2 * (ti.sin(0.06 * x + 0.11 * z) + ti.sin(0.09 * x + 0.07 * z))
-        if h >= 0:
-            for y in range(0, 3 + h):
-                set(ivec3(x, -64 + y, z), 1, vec3(0.25, 0.5, 0.25), 0.1)
-    for x, z in ti.ndrange((-64, 64), (-64, 64)):
-        for x1, z1 in ti.ndrange((x - 14, x + 15), (z - 14, z + 15)):
-            if (x1 >= -64 and x1 < 64 and z1 >= -64 and z1 < 64 and (x1 - x) ** 2 + (z1 - z) ** 2 <= 14 ** 2 and scene
-            .get_voxel(ivec3(x1, -64, z1))[0] > 0): set(ivec3(x, -63, z), 1, vec3(0.05, 0.1, 0.3), vec3(0, 0, 0.1))
-    for x, z in ti.ndrange((-64, 64), (-64, 64)):
-        if scene.get_voxel(ivec3(x, -63, z))[0] > 0: set(ivec3(x, -64, z), 1, vec3(0.05, 0.1, 0.3), vec3(0, 0, 0.1))
-        else: fill(ivec3(x, -64, z), ivec3(1, 3, 1), 1, vec3(0.25, 0.5, 0.25), 0.1)
-    for x, z in ti.ndrange((-64, 64), (-64, 64)):
-        for x1, z1 in ti.ndrange((x - 4, x + 5), (z - 4, z + 5)):
-            if (x1 >= -64 and x1 < 64 and z1 >= -64 and z1 < 64 and (x1 - x) ** 2 + (z1 - z) ** 2 <= 4 ** 2 and scene
-            .get_voxel(ivec3(x1, -62, z1))[0] == 0): fill(ivec3(x, -62, z), ivec3(1), 1, vec3(0.9, 0.7, 0.5), 0.1, True)
-    fill(ivec3(-64, -64, -64), ivec3(128, 1, 128), 1, vec3(0.7, 0.5, 0.3), 0.1)
 
 initialize(); scene.finish()
